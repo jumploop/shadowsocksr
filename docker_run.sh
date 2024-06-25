2ddd0409fe3fdf40c162eea2fb6cc3e273c9e79e
liming <827182486@qq.com>
2024-06-26 00:27:38 +0800

    优化脚本

--- docker_run.sh
+++ docker_run.sh
@@ -3,7 +3,7 @@ SERVER_PORT=51348
 
 rpm -q docker || yum -y install docker
 systemctl status docker || systemctl start docker
-
+systemctl is-enabled docker || systemctl enable docker
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
 docker rmi $(docker images -q)
