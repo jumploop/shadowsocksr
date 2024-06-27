901735f418f840e123284fd2a3df68a0a51b6d54
liming <827182486@qq.com>
2024-06-27 22:55:06 +0800

    优化代码

--- docker_run.sh
+++ docker_run.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 SERVER_PORT=51348
 
 rpm -q docker || yum -y install docker
@@ -9,4 +9,25 @@ docker rm $(docker ps -a -q)
 docker rmi $(docker images -q)
 
 docker build -t ssr .
-docker run -d  -p $SERVER_PORT:$SERVER_PORT ssr
+
+read -r -e -p "(默认: 1):" count
+[[ -z ${count} ]] && count=1
+
+create_docker() {
+  local port
+  port=$1
+  docker run -d -p "$port":$SERVER_PORT ssr
+  echo "container map port=$port"
+}
+
+number=0
+while [ "$number" -lt "$count" ]; do
+  echo "creating the number $number container"
+  if [ "$count" -eq 1 ]; then
+    PORT=$SERVER_PORT
+  else
+    PORT=$(python -c 'import random;print(random.randint(10000, 65536))')
+  fi
+  create_docker "$PORT"
+  number=$((number + 1))
+done
